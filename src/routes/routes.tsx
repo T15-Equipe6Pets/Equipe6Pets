c7a8448762b4a27f2aded1a23ee940389e2c480f
Renan.dev <dev.rfcsb@gmail.com>
2023-03-13 16:02:06 -0400

    feat/protector-router

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { Login } from "../pages/Login";
+import { ProtectedRoutes } from "../pages/ProtectRoute/ProtectRoute";
 import { Register } from "../pages/RegisterPage";
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/register" element={<Register />} />
-      <Route path="/home" element={<Home />} />
+      <Route path="/shop" element={<ProtectedRoutes />}>
+        <Route index element={<Home />} />
+      </Route>
+
       <Route path="/" element={<Login />} />
     </Routes>
   );
